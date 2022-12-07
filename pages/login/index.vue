f102c7abde0c8ba4e40ad3001905373b45dfdfae
Modena <73952306+FelipeModena@users.noreply.github.com>
2022-12-07 19:21:43 -0300

    login forms validation

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -7,11 +7,13 @@
 
           <b-row class="text-left">
             <b-col>
-              <b-form>
+              <b-form @submit="onSubmit">
                 <b-form-group label="Email" label-for="login-email">
                   <b-form-input
                     id="login-email"
                     v-model="email"
+                    required
+                    type="email"
                     placeholder="email@mail.com"
                     :formatter="formatter"
                     autofocus
@@ -21,17 +23,22 @@
                   <b-form-input
                     id="login-password"
                     v-model="password"
+                    required
                     type="password"
                     :formatter="formatter"
                   ></b-form-input>
                 </b-form-group>
+                <b-button type="submit" class="w-100" variant="primary"
+                  >Entrar</b-button
+                >
               </b-form>
             </b-col>
           </b-row>
           <b-row>
-            <b-col>
-              <b-button class="w-100" variant="primary">Entrar</b-button>
-              <p class="text-right text-secondary mt-1">Esqueci minha senha</p>
+            <b-col class="text-right mt-2">
+              <nuxt-link class="text-secondary" to="/">
+                Esqueci minha senha
+              </nuxt-link>
             </b-col>
           </b-row>
           <b-row class="mt-3">
@@ -39,7 +46,9 @@
               <p>ou</p>
               <p>
                 Precisa de uma conta?
-                <nuxt-link to="login/register"> Cadastre-se!</nuxt-link>
+                <nuxt-link class="text-secondary" to="login/register">
+                  Cadastre-se!</nuxt-link
+                >
               </p>
             </b-col>
           </b-row>
@@ -60,6 +69,10 @@ export default Vue.extend({
     }
   },
   methods: {
+    onSubmit(event: Event) {
+      event.preventDefault()
+      alert(JSON.stringify({}))
+    },
     formatter(value: string) {
       return value.toLowerCase()
     },
